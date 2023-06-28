eeffab5c3372e5ae0b6e3883f7aaff06360de21c
DESKTOP-DF6RQ5T\2dawi <2dawit70@gmail.com>
2023-06-28 09:19:13 +0900

    team 페이지 footer 생성

---
+++ src/main/java/multi/backend/project/controller/TeamController.java
@@ -0,0 +1,15 @@
+package multi.backend.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TeamController {
+
+    @GetMapping("/team")
+    public String footer(){
+        return "team";
+    }
+
+
+}
