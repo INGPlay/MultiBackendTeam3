6e700ea56cf72f969d54a9f349ed671165735ed0
김병주 <atker14@gmail.com>
2023-07-12 15:56:29 +0900

    jar to war

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/MultiBackendTeam3"
-JAR="$ROOT_PATH/application.jar"
+WAR="$ROOT_PATH/application.war"
 
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
@@ -9,11 +9,11 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/springboot-1.0.0-SNAPSHOT.war $JAR
+echo "[$NOW] $WAR 복사" >> $START_LOG
+cp $ROOT_PATH/build/libs/springboot-1.0.0-SNAPSHOT.war $WAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
-sudo nohup java -jar $JAR --spring.profiles.active=server  > $APP_LOG 2> $ERROR_LOG &
+echo "[$NOW] > $WAR 실행" >> $START_LOG
+nohup java -jar $WAR --spring.profiles.active=server  > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(pgrep -f $JAR)
+SERVICE_PID=$(pgrep -f $WAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
